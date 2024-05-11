fce99a1fc3059f6f36564abe5241b4fc3d7f279f
yougesh392 <ypinjani@gmail.com>
2024-05-11 01:38:35 -0400

    Initial commit

---
+++ src/main/java/com/usmobile/dailyusage/DailyusageApplication.java
@@ -0,0 +1,13 @@
+package com.usmobile.dailyusage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyusageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyusageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/usmobile/dailyusage/DailyusageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usmobile.dailyusage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyusageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
