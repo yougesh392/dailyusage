0a615290b5a718aed303640a76da28ca32a7b457
yougesh392 <ypinjani@gmail.com>
2024-05-19 20:27:15 -0400

    added the scheduling annotation

--- src/main/java/com/usmobile/dailyusage/DailyusageApplication.java
+++ src/main/java/com/usmobile/dailyusage/DailyusageApplication.java
@@ -2,8 +2,10 @@ package com.usmobile.dailyusage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class DailyusageApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DailyusageApplication.class, args);
